155380b70324d365880abfaa9518c9c9d40dab24
sarsena <sjarsena@gmail.com>
2014-04-03 14:45:24 -0400

    Error handling

--- lib/active_shipping/shipping/carriers/ups.rb
+++ lib/active_shipping/shipping/carriers/ups.rb
@@ -173,7 +173,7 @@ module ActiveMerchant
         access_request = "<?xml version='1.0' ?>" + build_access_request
         void_request = "<?xml version='1.0' encoding='UTF-8' ?>" + build_void_label_request(identification_number, tracking_numbers, options)
         response = commit(:void, save_request(access_request + void_request), (options[:test] || false))
-        parse_void_response(response, options)
+        p parse_void_response(response, options)
       end
 
       protected
