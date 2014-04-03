91cc93ce7a94bea085e94a432d052f0876274ef5
sarsena <sjarsena@gmail.com>
2014-04-03 16:59:26 -0400

    Error handling

--- lib/active_shipping/shipping/carriers/ups.rb
+++ lib/active_shipping/shipping/carriers/ups.rb
@@ -174,11 +174,11 @@ module ActiveMerchant
           access_request = "<?xml version='1.0' ?>" + build_access_request
           void_request = "<?xml version='1.0' encoding='UTF-8' ?>" + build_void_label_request(identification_number, tracking_numbers, options)
           response = commit(:void, save_request(access_request + void_request), (options[:test] || false))
-          
+          p "Start"
           xml = parse_void_response(response, options)
           success = response_success?(xml)
           message = response_message(xml)
-
+          p "Hi"
           raise message unless success
           xml
         rescue RuntimeError => e
@@ -708,7 +708,7 @@ module ActiveMerchant
         else 
           p "Hello"
         end
-        
+        p "End"
         VoidResponse.new(success, message, Hash.from_xml(response).values.first,
           {
             :xml => response,
