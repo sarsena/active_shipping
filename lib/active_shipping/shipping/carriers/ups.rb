4b27755c95165cefd505526c0d81f36653da69c4
sarsena <sjarsena@gmail.com>
2014-04-03 14:55:07 -0400

    Error handling

--- lib/active_shipping/shipping/carriers/ups.rb
+++ lib/active_shipping/shipping/carriers/ups.rb
@@ -170,10 +170,15 @@ module ActiveMerchant
       def void_shipping_labels(identification_number, tracking_numbers=[], options={})
         options = @options.merge(options)
         tracking_numbers = Array(tracking_numbers)
-        access_request = "<?xml version='1.0' ?>" + build_access_request
-        void_request = "<?xml version='1.0' encoding='UTF-8' ?>" + build_void_label_request(identification_number, tracking_numbers, options)
-        response = commit(:void, save_request(access_request + void_request), (options[:test] || false))
-        p parse_void_response(response, options)
+        begin
+          access_request = "<?xml version='1.0' ?>" + build_access_request
+          void_request = "<?xml version='1.0' encoding='UTF-8' ?>" + build_void_label_request(identification_number, tracking_numbers, options)
+          response = commit(:void, save_request(access_request + void_request), (options[:test] || false))
+          
+          parse_void_response(response, options)
+        rescue RuntimeError => e
+          raise "Could not void shipping label. #{e.message}"
+        end
       end
 
       protected
